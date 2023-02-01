7b0f241aa5e0defcf3af4c4e588433ad3a0e0cfc
sureshm <jvap2@uwyo.edu>
2023-02-01 16:21:39 -0700

    Bidiag

--- SAXPY/SVD/SVDCPU.cpp
+++ SAXPY/SVD/SVDCPU.cpp
@@ -52,11 +52,35 @@ void Update_A_1(float* A, float* u, float* p_hold, float* p_hold_2, int k, int n
             p_hold_2[i*nx+j]=-2*u[i]*p_hold[j];
         }
     }
-    for(int i=k; i<nx;i++){
-        for(int j=k;j<ny;j++){
+    for(int i=k; i<ny;i++){
+        for(int j=k;j<nx;j++){
             A[i*nx+j]+=p_hold_2[i*nx+j];
         }
     }
+}
+
+
+void Update_A_2(float* A, float* v, float* p_hold, float* p_hold_2, int k, int ny, int nx){
+    float temp{};
+    for(int i=k; i<ny;i++){
+        for(int j=k+1; j<nx; j++){
+            temp+=A[i*nx+j]*v[j];
+        }
+        p_hold[i]=-2*temp;
+        temp=0;
+    }
+    for(int i=k; i<ny;i++){
+        for(int j=k+1; j<nx; j++){
+            p_hold_2[i*nx+j]=p_hold[i]*v[j];
+        }
+    }
+    for(int i=k; i<ny;i++){
+        for(int j=k;j<nx;j++){
+            A[i*nx+j]+=p_hold_2[i*nx+j];
+        }
+    }
+
+
 }
 
 
@@ -64,8 +88,11 @@ void Bidiagonal(float* A, float* x, float* u, float* v, float* e_1, float* e_1_u
     float s_x_1{};
     float x_norm{};
     float u_norm{};
+    float v_norm{};
     float p_hold[ny];
     float p_hold_2[ny];
+    float p_hold_3[ny];
+    float p_hold_4[ny];
     for(int k{}; k<nx; k++){
         for(int i=k; i<ny; i++){
             x[i]=A[i*ny+k];
@@ -80,7 +107,13 @@ void Bidiagonal(float* A, float* x, float* u, float* v, float* e_1, float* e_1_u
         if(k<=nx-2){
             for(int i=k+1; i<nx;i++){
                 x[i]=A[k*nx+i];
-                
+                s_x_1=sign(x[k]);
+                L_2(x,ny,x_norm);
+                Vect_Const_Mult(e_1,e_1_v,s_x_1*x_norm,ny);
+                cpuVectorAddition(x,e_1_u,u,ny);
+                L_2(v,ny,v_norm);
+                Vect_Const_Mult_Addr(v,1.0f/v_norm,ny);
+                Update_A_2(A, v, p_hold_3, p_hold_4, k, ny, nx);
             }
         }
     }
