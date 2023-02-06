e5c1405bea62f3a171e7119072067b56cc53a5f6
sureshm <jvap2@uwyo.edu>
2023-02-06 13:42:48 -0700

    CUDA Bidiag

---
+++ SAXPY/SVD/SVDGPU.cu
@@ -0,0 +1,11 @@
+#include "SVD.h"
+#include "GPUErrors.h"
+
+
+__global__ void Bidiag(float* A, float* p, float* p_2, float* res_1, float* res_2, float* v_1, float* v_2, int ny, int nx){
+
+}
+
+__host__ void Bidiag_Helper(float* A, float* ref, float* p, float* p_2, float* res_1, float* res_2, float* v_1, float* v_2, int ny, int nx){
+    
+}
