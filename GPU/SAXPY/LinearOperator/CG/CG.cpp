36d9e9ef8494023ef88be54d5be6d6162e3ef772
sureshm <jvap2@uwyo.edu>
2023-03-29 07:01:15 -0600

    Beginning Linear Operators

---
+++ GPU/SAXPY/LinearOperator/CG/CG.cpp
@@ -0,0 +1,82 @@
+#include "CG.h"
+#include "GPUErrors.h"
+
+
+int main(){
+    float *A,*A_T,*A_res, *b, *b_res, *r, *r_old, *d, *d_old, *x, *x_old, *Ax, *b_check;
+    float *r_GPU,*r_old_GPU,*d_GPU,*d_old_GPU,*x_GPU,*x_old_GPU;
+    float lambda{}, beta{};
+    float lambda_GPU{}, beta_GPU{};
+    int ny=4096;
+    int nx=4096;
+    if(ny!=nx){
+        return -1;
+    }
+    cout<<"Size="<<ny<<endl;
+    Ax=new float[ny];
+    A=new float[ny*nx];
+    A_T=new float[ny*nx];
+    A_res=new float[ny*nx];
+    b=new float[ny];
+    b_res=new float[ny];
+    r=new float[ny];
+    r_old=new float[ny];
+    d=new float[ny];
+    d_old=new float[ny];
+    x=new float[ny];
+    x_old=new float[ny];
+    r_GPU=new float[ny];
+    r_old_GPU=new float[ny];
+    d_GPU=new float[ny];
+    d_old_GPU=new float[ny];
+    x_GPU=new float[ny];
+    x_old_GPU=new float[ny];
+    b_check=new float[ny];
+    int iter{};
+    InitializeMatrix(A,ny,nx);
+    Diag_Dominant_Opt(A,ny);
+    TransposeOnCPU(A,A_T,ny,ny);
+    Generate_Vector(b,ny);
+    cpuMatrixMult(A,A_T,A_res,ny,nx);
+    cpuMatrixVect(A_T,b,b_res,ny,nx);
+    Generate_Vector(x_old,ny);
+    cpuMatrixVect(A_res, x_old, Ax, ny, nx);
+    vector_subtract(b_res,Ax,r_old,ny);
+    vector_subtract(b_res,Ax,r_old_GPU,ny);
+
+    for(int i=0; i<ny;i++){
+        x_old_GPU[i]=x_old[i];
+        d_old[i]=r_old[i];
+        d_old_GPU[i]=d_old[i];
+    }
+    chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    C_G(A_res,r,r_old,d,d_old,x,x_old,beta,lambda,ny,&iter);
+    end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elasped_seconds = end - start;
+	cout << "CPU Execution time: " << (elasped_seconds.count() * 1000.0f) << " msecs" << endl;
+    CG_Helper(A_res,x,r_GPU,r_old_GPU,d_GPU,d_old_GPU,x_GPU,x_old_GPU,beta_GPU,lambda_GPU,ny,iter);
+
+    delete[] Ax;
+    delete[] A;
+    delete[] A_T;
+    delete[] A_res;
+    delete[] b;
+    delete[] b_res;
+    delete[] r;
+    delete[] r_old;
+    delete[] d;
+    delete[] d_old;
+    delete[] x;
+    delete[] x_old;
+    delete[] b_check;
+    delete[] r_GPU;
+    delete[] r_old_GPU;
+    delete[] d_GPU;
+    delete[] d_old_GPU;
+    delete[] x_GPU;
+    delete[] x_old_GPU;
+
+    return 0;
+
+}
---
+++ GPU/SAXPY/LinearOperator/CG/CGCPU.cpp
@@ -0,0 +1,43 @@
+#include "CG.h"
+
+void C_G(float* A, float* r, float* r_old, float* d, float* d_old, float* x, float* x_old, float beta, float lamdba, int size, int* iter){
+    float Ad[size]={};
+    float lamd_d[size]={};
+    float beta_d[size]={};
+    float lambd_AD[size]={};
+    float temp_1{};
+    float temp_2{};
+    *iter=1;
+    int MaxIter=10*size;
+    float fSum;
+    float norm{};
+    while(*(iter)<MaxIter){
+        cpuMatrixVect(A,d_old,Ad,size,size);
+        temp_1=Dot_Product(r_old,r_old,size);
+        temp_2=Dot_Product(d_old,Ad,size);
+        if(fabsf(temp_2)<1e-8){
+            return;
+        }
+        lamdba=temp_1/temp_2;
+        float neg_lamb=-lamdba;
+        Const_Vect_Mult(d_old,lamd_d,lamdba,size);
+        Const_Vect_Mult(Ad,lambd_AD,neg_lamb,size);
+        cpuVectorAddition(x_old,lamd_d,x,size);
+        cpuVectorAddition(r_old,lambd_AD,r,size);
+        norm=L_2(r,size);
+        if(norm<1e-6){
+            return;
+        }
+        temp_2=Dot_Product(r,r,size);
+        beta=temp_2/temp_1;
+        Const_Vect_Mult(d_old,beta_d,beta,size);
+        cpuVectorAddition(r,beta_d,d,size);
+        for(int i=0;i<size;i++){
+            d_old[i]=d[i];
+            r_old[i]=r[i];
+            x_old[i]=x[i];
+
+        }
+        *(iter)+=1;
+    }
+}
