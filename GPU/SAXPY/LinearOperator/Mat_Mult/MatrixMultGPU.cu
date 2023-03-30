96268b4b6b65a000007f32bc78182d6ce203ca70
sureshm <jvap2@uwyo.edu>
2023-03-30 04:26:54 -0600

    Linear Operator

--- GPU/SAXPY/LinearOperator/Mat_Mult/MatrixMultGPU.cu
+++ GPU/SAXPY/LinearOperator/Mat_Mult/MatrixMultGPU.cu
@@ -20,6 +20,7 @@ __global__ void NaiveMult(float* g_A, float* g_B, float* g_C, const int ny, cons
 
 __host__ void gpuMultHelper(float* h_A, float* h_B, float* h_C, float* ref, const int ny, const int nx)
 {
+	float ElapsedTime{}, Elapsed_hd{}, Elapsed_dh{};
 	float* d_A, * d_B, * d_C;
 	const int MatrixSizeInBytes = nx * sizeof(float);//rowsxcolsxnumber of bytes
 	const int C_size_in_Bytes=sizeof(float)*(nx*(ny));
@@ -29,11 +30,17 @@ __host__ void gpuMultHelper(float* h_A, float* h_B, float* h_C, float* ref, cons
 	HandleCUDAError(cudaMalloc((void**)&d_A, MatrixSizeInBytes));
 	HandleCUDAError(cudaMalloc((void**)&d_B, MatrixSizeInBytes));
 	HandleCUDAError(cudaMalloc((void**)&d_C, C_size_in_Bytes));
-
-
+    cudaEvent_t start, stop;
+    cudaEvent_t start_dh, stop_dh;
+    cudaEvent_t start_hd, stop_hd;
+	HandleCUDAError(cudaEventCreate(&start));
+	HandleCUDAError(cudaEventCreate(&stop));
+    HandleCUDAError(cudaEventCreate(&start_dh));
+    HandleCUDAError(cudaEventCreate(&stop_dh));
+    HandleCUDAError(cudaEventCreate(&start_hd));
+    HandleCUDAError(cudaEventCreate(&stop_hd));
+    HandleCUDAError(cudaEventRecord(start_hd,0));
 	//transfer data from CPU Memory to GPU Memory
-	chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
 	if(!HandleCUDAError(cudaMemcpy(d_A, h_A, MatrixSizeInBytes, cudaMemcpyHostToDevice))){
 		cout<<"cannot transfer A"<<endl;
 	}
@@ -41,10 +48,17 @@ __host__ void gpuMultHelper(float* h_A, float* h_B, float* h_C, float* ref, cons
 	if(!HandleCUDAError(cudaMemcpy(d_B, h_B, MatrixSizeInBytes, cudaMemcpyHostToDevice))){
 		cout<<"cannot transfer B"<<endl;
 	}
-	end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	cout << "GPU Memory Transfer time (H to D): " << (elapsed_seconds.count() * 1000.0f) << " msecs" << endl;
-
+	if (!HandleCUDAError(cudaEventRecord(stop_hd, 0))) {
+		cout << "Unable to perform event records for stop" << endl;
+	}
+	//Synchronize the stop event
+	if (!HandleCUDAError(cudaEventSynchronize(stop_hd))) {
+		cout << "Unable to perform stream synch with stream_4" << endl;
+	}
+	//Save the elapsed time
+	if (!HandleCUDAError(cudaEventElapsedTime(&Elapsed_hd, start_hd, stop_hd))) {
+		cout << "Unable to find elapsed time between events" << endl;
+	}
 	//Kernel Invoke Parameters - 2D Grid and 2D Blocks
 	int dimx = 16;
 	int dimy = 16;
@@ -52,33 +66,54 @@ __host__ void gpuMultHelper(float* h_A, float* h_B, float* h_C, float* ref, cons
 	//Never use the maximum number of threads in a block, the standard value is 256 or less
 	dim3 grid((ny+block.y-1)/block.y, (nx + block.x - 1) / block.x);//First says the number of blocks in the y direction, this ensures number of blocks can handle data
 	//We do not need to cast since the structure is with ints
-	cout << "\t2D Grid Dimension" << endl;
-	cout << "\tNumber of Blocks along X dimension: " << grid.x << endl;
-	cout << "\tNumber of Blocks along Y dimension: " << grid.y << endl;
-	cout << "\t2D Block Dimension" << endl;
-	cout << "\tNumber of threads along X dimension: " << block.x << endl;
-	cout << "\tNumber of threads along Y dimension: " << block.y << endl;
+	// cout << "\t2D Grid Dimension" << endl;
+	// cout << "\tNumber of Blocks along X dimension: " << grid.x << endl;
+	// cout << "\tNumber of Blocks along Y dimension: " << grid.y << endl;
+	// cout << "\t2D Block Dimension" << endl;
+	// cout << "\tNumber of threads along X dimension: " << block.x << endl;
+	// cout << "\tNumber of threads along Y dimension: " << block.y << endl;
 
 	//Launch Multiplication kernel
-	start = std::chrono::system_clock::now();
+	HandleCUDAError(cudaEventRecord(start,0));
 	NaiveMult << <grid, block >> > (d_A,d_B,d_C,ny,nx);
 	cudaDeviceSynchronize();//Recall this makes the CPU wait
-	end = std::chrono::system_clock::now();
-	elapsed_seconds = end - start;
-	cout << "Naive Multiplication: GPU Execution time: " << (elapsed_seconds.count() * 1000.0f) << " msecs" << endl;
-
-	start = std::chrono::system_clock::now();
+    if (!HandleCUDAError(cudaEventRecord(stop, 0))) {
+		cout << "Unable to perform event records for stop" << endl;
+	}
+	//Synchronize the stop event
+	if (!HandleCUDAError(cudaEventSynchronize(stop))) {
+		cout << "Unable to perform stream synch with stream_4" << endl;
+	}
+	//Save the elapsed time
+	if (!HandleCUDAError(cudaEventElapsedTime(&ElapsedTime, start, stop))) {
+		cout << "Unable to find elapsed time between events" << endl;
+	}
+	HandleCUDAError(cudaEventRecord(start_dh,0));
 	//Copy product matrix to host
 	if(!HandleCUDAError(cudaMemcpy(h_C, d_C, C_size_in_Bytes, cudaMemcpyDeviceToHost))){
 		cout<<"Unable to transfer C"<<endl;
 	}
-	end = std::chrono::system_clock::now();
-	elapsed_seconds = end - start;
-	cout << "GPU Memory Transfer time (D to H): " << (elapsed_seconds.count() * 1000.0f) << " msecs" << endl;
+	if (!HandleCUDAError(cudaEventRecord(stop_dh, 0))) {
+		cout << "Unable to perform event records for stop" << endl;
+	}
+	//Synchronize the stop event
+	if (!HandleCUDAError(cudaEventSynchronize(stop_dh))) {
+		cout << "Unable to perform stream synch with stream_4" << endl;
+	}
+	//Save the elapsed time
+	if (!HandleCUDAError(cudaEventElapsedTime(&Elapsed_dh, start_dh, stop_dh))) {
+		cout << "Unable to find elapsed time between events" << endl;
+	}
 	
 	//Verification code
 	Verify(h_C,ref, ny);
-	
+	float total_time=Elapsed_dh+Elapsed_hd;
+    float bytes_transferred=(2*MatrixSizeInBytes+C_size_in_Bytes)*1.0f;
+    float throughput=(bytes_transferred*1e-6)/(total_time);
+    cout<< "GPU MM Memory elapsed time: "<<total_time<< " ms"<<endl;
+    cout<< "GPU MM Exec elapsed time: "<<ElapsedTime<< " ms"<<endl;
+    cout<< "GPU MM total elapsed time: "<<ElapsedTime+total_time<< " ms"<<endl;
+    cout<<"GPU throughput: "<<throughput<< "GB/s"<<endl;
 	//Release Memory and reset device
 	HandleCUDAError(cudaFree(d_A));
 	HandleCUDAError(cudaFree(d_B));
--- GPU/SAXPY/LinearOperator/Mat_Mult/README.md
+++ GPU/SAXPY/LinearOperator/Mat_Mult/README.md
@@ -1,2 +1,3 @@
 |Size|CPU exec time(ms)|GPU exec time(ms)|Memory Transfer (ms)|SpeedUp|Throughput|
 |----|-----------------|-----------------|--------------------|-------|----------|
+|256| 
